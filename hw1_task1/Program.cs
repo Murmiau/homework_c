79a8096c38730e9f0127fff33726f60aa821bf9d
Vera Mirgorodscaya <mirgorodscya@mail.ru>
2023-01-27 22:12:40 +0300

    task1

---
+++ hw1_task1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+Console.Clear();
+Console.Write("Введите первое число: ");
+int a=int.Parse(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int b=int.Parse(Console.ReadLine());
+if (a > b)
+    Console.WriteLine($"max = {a}, min = {b}");
+else if (b > a)
+    Console.WriteLine($"max = {b}, min = {a}");
+else
+    Console.WriteLine("Числа равны!");
