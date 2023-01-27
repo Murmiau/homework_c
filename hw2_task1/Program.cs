bf4a50f72b4b03c147b9822aa94a06e75a054849
Vera Mirgorodscaya <mirgorodscya@mail.ru>
2023-01-27 22:18:18 +0300

    task1

---
+++ hw2_task1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using static System.Console;
+
+Clear();
+Write("Введите трехзначное число: ");
+int a=int.Parse(ReadLine());
+
+if ((a>99)&&(a<1000))
+{
+    int result=a / 10 % 10;
+    WriteLine($"{a}->{result}");
+}
+else
+{
+    Write($"Вы ввели не трехзначное число!");
+}
