19b624eb1287a6286e0a0483a73c60fc3943656c
Vera Mirgorodscaya <mirgorodscya@mail.ru>
2023-01-27 22:12:57 +0300

    task3

---
+++ hw1_task3/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+Console.Clear();
+Console.Write("Введите число: ");
+int a=int.Parse(Console.ReadLine());
+int b=a % 2;
+if (b == 0)
+    Console.WriteLine($"{a} - четное число!");
+else
+    Console.WriteLine($"{a} - нечетное число!");
