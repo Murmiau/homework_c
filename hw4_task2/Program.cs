98a3f13319e2418bb4b3a3316ee78c0e5f2186ae
Vera Mirgorodscaya <mirgorodscya@mail.ru>
2023-01-30 02:55:28 +0300

    task2

---
+++ hw4_task2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using static System.Console;
+
+Clear();
+Write("Введите число: ");
+int a=int.Parse(ReadLine());
+int len=Lennumber(a);
+
+int Lennumber(int a)
+{
+    int len=0;
+    while(a>0)
+    {
+        a/=10;
+        len++;
+    }
+    return len;
+}
+int Summnumber(int a, int len)
+{
+    int summ=0;
+    for (int i=1; i<=len; i++)
+    {
+        summ += a%10;
+        a/=10;
+    }
+    return summ;
+}
+int myresult=Summnumber(a, len);
+Write($"Сумма цифр в числе: {myresult}");
