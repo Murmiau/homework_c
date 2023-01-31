50fb8b1936053ad9e129c09985ea1f20e44f4b10
Vera Mirgorodscaya <mirgorodscya@mail.ru>
2023-01-31 23:57:33 +0300

    task3

---
+++ hw5_task3/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using static System.Console;
+
+Clear();
+int[] array=GetArray(10, 0, 100);
+WriteLine(String.Join(",",array));
+int i=0;
+int max=0;
+int min=100;
+foreach(int item in array)
+{
+    if (max<item)
+    {
+        max=item;
+        i++;
+    }
+    else if (min>item)
+    {
+        min=item;
+        i++;
+    }
+    else
+    {
+        i++;
+    }
+}
+int myresult=max-min;
+WriteLine($"Разница между максимальным и минимальным элементом: {myresult}");
+int[] GetArray(int size, int min, int max)
+{
+    int[] result=new int[size];
+    for(int i=0; i<size; i++)
+    {
+        result[i]=new Random().Next(min, max+1);
+    }
+    return result;
+}
